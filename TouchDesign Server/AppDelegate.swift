f8d17e65fc48540cf4046682c975affac2aafba1
Harry Shamansky <harry.shamansky@icloud.com>
2015-09-29 20:04:04 -0400

    Initial Commit

---
+++ TouchDesign Server/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TouchDesign Server
+//
+//  Created by Harry Shamansky on 9/29/15.
+//  Copyright © 2015 Harry Shamansky. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TouchDesign Server/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TouchDesign Server
+//
+//  Created by Harry Shamansky on 9/29/15.
+//  Copyright © 2015 Harry Shamansky. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
